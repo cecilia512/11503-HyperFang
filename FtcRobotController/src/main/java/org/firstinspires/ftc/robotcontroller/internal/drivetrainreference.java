aecefd083b778de70ce3a643c8b5868db044fb62
cecilia512 <ceciliacskinner05@gmail.com>
2022-11-14 16:11:29 -0600

    references

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/drivetrainreference.java
@@ -0,0 +1,243 @@
+package Library;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class drivetrainreference {
+
+    LinearOpMode opMode;
+
+    public double countsPerInch;
+
+    private DcMotor motorFL;
+    private DcMotor motorFR;
+    private DcMotor motorBL;
+    private DcMotor motorBR;
+  //  private Sensors sensor;
+    ElapsedTime time;
+
+    public Drivetrain(LinearOpMode opMode) throws InterruptedException {
+        this.opMode = opMode;
+        motorFL = this.opMode.hardwareMap.dcMotor.get("fL");
+        motorFR = this.opMode.hardwareMap.dcMotor.get("fR");
+        motorBL = this.opMode.hardwareMap.dcMotor.get("bL");
+        motorBR = this.opMode.hardwareMap.dcMotor.get("bR");
+
+        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFR.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBR.setDirection(DcMotorSimple.Direction.FORWARD);
+        countsPerInch = EncodersPerInch(560, .5, (75/25.4));
+        time = new ElapsedTime();
+     //   sensor = new Sensors(this.opMode);
+    }
+
+    public void startMotors(double left, double right) {
+        motorFL.setPower(left);
+        motorFR.setPower(right);
+        motorBL.setPower(left);
+        motorBR.setPower(right);
+    }
+
+    public void stopMotors() {
+        motorFL.setPower(0);
+        motorFR.setPower(0);
+        motorBL.setPower(0);
+        motorBR.setPower(0);
+    }
+
+    public void moveForward(double power) {
+        startMotors(power, power);
+    }
+
+    public double EncodersPerInch(double encoders, double gearReduction, double wheelDiameter){
+        return ((encoders * gearReduction) /(wheelDiameter * Math.PI) );
+    }
+
+    public void resetEncoders(){
+        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        opMode.idle();
+        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        opMode.idle();
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        opMode.idle();
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        opMode.idle();
+
+        motorFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        opMode.idle();
+        motorFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        opMode.idle();
+        motorBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        opMode.idle();
+        motorBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        opMode.idle();
+    }
+
+    public void setDTBrake(){
+        motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        opMode.idle();
+        motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        opMode.idle();
+        motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        opMode.idle();
+        motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        opMode.idle();
+    }
+
+    public void setDTFloat(){
+        motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        opMode.idle();
+        motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        opMode.idle();
+        motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        opMode.idle();
+        motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        opMode.idle();
+    }
+
+    public double getEncoderAvg(){
+        int count = 4;
+        if (motorFL.getCurrentPosition() == 0){
+            count--;
+        }
+        if (motorFR.getCurrentPosition() == 0){
+            count--;
+        }
+        if (motorBL.getCurrentPosition() == 0){
+            count--;
+        }
+        if (motorBR.getCurrentPosition() == 0){
+            count--;
+        }
+        if (count == 0) count++;
+        return (Math.abs(motorFL.getCurrentPosition()) +
+                Math.abs(motorFR.getCurrentPosition()) +
+                Math.abs(motorBL.getCurrentPosition()) +
+                Math.abs(motorBR.getCurrentPosition())) / count;
+    }
+
+    public void moveInches(double inches, double power){
+        resetEncoders();
+        while (getEncoderAvg() < inches * countsPerInch && opMode.opModeIsActive()){
+            startMotors(power, power);
+        }
+        stopMotors();
+    }
+
+    public void moveTime(int millis, double power){
+        time.reset();
+        while (time.milliseconds() < millis){
+            startMotors(power, power);
+        }
+        stopMotors();
+    }
+
+    public void turnP(double angle, double p) {
+        double kP = p;
+        final double startPos = sensor.getGyroYaw(); // initial angle
+        final double angleDiff = angle - startPos; // desired angle - initial angle = difference
+        double deltaAngle = sensor.getTrueDiff(angle); // difference in current and desired angle (negative if need to turn left and positive if need to turn right)
+        double changePID = 0; // the power we will apply to our motors
+        while(Math.abs(deltaAngle) > .3){
+            deltaAngle = sensor.getTrueDiff(angle);
+            changePID = ((deltaAngle/Math.abs(angleDiff)) * kP); // As we get closer to the desired angle, the value that is multiplied with kP is decreased (we won't overshoot)
+            if (changePID < 0){ // if this value is negative (need to turn left)
+                startMotors(changePID - .075 , -changePID + .075); // turn method for going left (left side turns backwards and right side turns forward)
+            }
+            else{ // if changePID is positive (need to turn right)
+                startMotors(changePID + .075, -changePID - .075); // turn method for going right (left side turns forward and right side turns backward)
+            }
+            // the constants that are added or subtracted with changePID in startMotors allow the motors to turn when the value of changePID becomes small
+            /*opMode.telemetry.addData("Current position", sensor.getGyroYaw());
+            opMode.telemetry.addData("Current Difference",deltaAngle);
+            opMode.telemetry.addData("changePID", changePID);
+            opMode.telemetry.addData("angleDiff: ", angleDiff);
+            opMode.telemetry.update();
+             */
+        }
+        stopMotors();
+    }
+
+    public void turnPD(double angle, double p, double d, double timeout){
+        time.reset();
+        double kP = p / 90;
+        double kD = d;
+        double currentTime = time.milliseconds();
+        double pastTime = 0;
+        double prevAngleDiff = sensor.getTrueDiff(angle);
+        double angleDiff = prevAngleDiff;
+        double changePID = 0;
+        while (Math.abs(angleDiff) > .5 && time.milliseconds() < timeout && opMode.opModeIsActive()) {
+            pastTime = currentTime;
+            currentTime = time.milliseconds();
+            double dT = currentTime - pastTime;
+            angleDiff = sensor.getTrueDiff(angle);
+            changePID = (angleDiff * kP) + ((angleDiff - prevAngleDiff) / dT * kD); // second part is rate of change of error (derivative)
+            if (changePID < 0) {
+                startMotors(changePID - .10, -changePID + .10);
+            } else {
+                startMotors(changePID + .10, -changePID - .10);
+            }
+            opMode.telemetry.addData("P", (angleDiff * kP));
+            opMode.telemetry.addData("D", ((Math.abs(angleDiff) - Math.abs(prevAngleDiff)) / dT * kD));
+            opMode.telemetry.addData("Gyro Roll: ", sensor.getGyroRoll());
+            opMode.telemetry.addData("Gyro Yaw: ", sensor.getGyroYaw());
+            opMode.telemetry.addData("Gyro Pitch: ", sensor.getGyroPitch());
+            opMode.telemetry.update();
+            prevAngleDiff = angleDiff;
+        }
+        stopMotors();
+    }
+
+    public void turnPI(double angle, double p, double i, double timeout) {
+        time.reset();
+        double kP = p / 90;
+        double kI = i / 100000;
+        double currentTime = time.milliseconds();
+        double pastTime = 0;
+        double P = 0;
+        double I = 0;
+        double angleDiff = sensor.getTrueDiff(angle);
+        double changePID = 0;
+        while (Math.abs(angleDiff) > .5 && time.milliseconds() < timeout && opMode.opModeIsActive()) {
+            pastTime = currentTime;
+            currentTime = time.milliseconds();
+            double dT = currentTime - pastTime;
+
+            angleDiff = sensor.getTrueDiff(angle); // error
+            P = angleDiff * kP; // power is proportional to error
+            I += dT * angleDiff * kI; // sum of error throughout entire duration of the turn
+            changePID = P;
+            changePID += I;
+            opMode.telemetry.addData("PID: ", changePID);
+            opMode.telemetry.addData("diff", angleDiff);
+            opMode.telemetry.addData("P", P);
+            opMode.telemetry.addData("I", I);
+            opMode.telemetry.addData("Gyro Yaw: ", sensor.getGyroYaw());
+            opMode.telemetry.addData("motorBR power: ", motorBR.getPower());
+            opMode.telemetry.update();
+            if (changePID < 0) {
+                startMotors(changePID - .1, -changePID + .1);
+            } else {
+                startMotors(changePID + .1, -changePID - .1);
+            }
+        }
+        stopMotors();
+    }
+
+    public void turnPID(double angle, double p, double i, double d, double timeout) {
+        time.reset();
+        double kP = p / 90;
+        double kI = i / 100000;
+        double kD = d;
+        double currentTime = time.milliseconds();
+        double pastTime = 0;
+
+    }
+
+
+
+
---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/teleopreference.java
@@ -0,0 +1,265 @@
+
+
+package Opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="TeleOp", group="4546")
+public class Teleop extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor fL, bL, fR, bR; // front left, back left, front right, back right
+    private Double leftDrive = null; // left  drive call for other motors
+    private Double rightDrive = null; // right drive call for other motors
+    private DcMotor intake = null; // intake motor
+    private DcMotor outake = null; // outputting unnecessary
+    private Servo sort = null; // sorting items collected
+    private DcMotor spin = null; // carousel control motor
+    private DcMotor arm = null; // pulley motor
+    private Servo wrist = null; // wrist like outake
+    private Servo shippingArm = null;
+    private Servo hook = null;
+
+
+
+
+
+
+    public double WeightAvg(double x, double y, double z) {
+        double speed_D = 0;
+
+        if ((Math.abs(x) + Math.abs(y) + Math.abs(z))  != 0.0) {
+            speed_D = ((x * Math.abs(x)) + (y * Math.abs(y)) + (z * Math.abs(z)))
+                    / (Math.abs(x) + Math.abs(y) + Math.abs(z));
+        }
+        return (speed_D);
+    }
+
+    public void driveTrainPower(double forward, double strafe, double rotate){
+        fL.setPower(WeightAvg(forward,strafe,-rotate));
+        fR.setPower(WeightAvg(forward,-strafe,rotate));
+        bL.setPower(WeightAvg(forward,-strafe,-rotate));
+        bR.setPower(WeightAvg(forward,strafe,rotate));
+    }
+
+
+
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        fL  = hardwareMap.get(DcMotor.class, "fL");
+        bL  = hardwareMap.get(DcMotor.class, "bL");
+        fR  = hardwareMap.get(DcMotor.class, "fR");
+        bR  = hardwareMap.get(DcMotor.class, "bR");
+        intake  = hardwareMap.get(DcMotor.class, "intake");
+        outake = hardwareMap.get(DcMotor.class, "outake");
+        spin = hardwareMap.get(DcMotor.class, "carousel");
+        arm = hardwareMap.get(DcMotor.class, "arm");
+        wrist = hardwareMap.get(Servo.class, "wrist");
+        sort = hardwareMap.get(Servo.class, "sort");
+        shippingArm = hardwareMap.get(Servo.class, "shippingArm");
+        hook = hardwareMap.get(Servo.class, "hook");
+        fL.setDirection(DcMotor.Direction.REVERSE);
+        bL.setDirection(DcMotor.Direction.REVERSE);
+        fR.setDirection(DcMotor.Direction.FORWARD);
+        bR.setDirection(DcMotor.Direction.FORWARD);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Variables to store positions
+        double sPos = .05;
+        double hPos = .05;
+        String liftState = "down";
+        double wristDeposit = .6;
+        double wristRest = .15;
+        double wristTilt = .3;
+        double wristGround = .8;
+        double liftMax = 1250;
+        double liftMin = 0;
+
+        // Waits for the game to start (driver presses PLAY)
+
+        waitForStart();
+        runtime.reset();
+
+        // starting program
+        double power = 1.0;
+        shippingArm.setPosition(.05);
+        hook.setPosition(.05);
+
+        while (opModeIsActive()) {
+            //Driver 1
+            /*
+            Driver 1 controls the drivetrain, carousel, intake, sorter, and the base level
+            outtake. This ensures the maximum efficiency and speed possible as less coordination is
+            needed between drivers for when it matters such as competing for tipping the shared
+            shipping hub. Additionally, driver 1 should be able to spin the carousel wheel in either
+            direction regardless of team color.
+//////////////////////////////////////////////////////////////////////////////////////////////////
+            POSSIBLE FUTURE CHANGES:
+            Single button Macro
+             */
+
+            // Driving [arcade mode]
+            if (Math.abs(gamepad1.left_stick_y) > .05 || Math.abs(gamepad1.left_stick_x) > .05 || Math.abs(gamepad1.right_stick_x) > .05) {
+                driveTrainPower(-power*gamepad1.left_stick_y, -power*gamepad1.left_stick_x, -power*gamepad1.right_stick_x * .78);
+            } else {
+                driveTrainPower(0, 0, 0);
+            }
+
+            if (gamepad1.dpad_down) // Half-power mode
+                power = 0.5;
+            else if (gamepad1.dpad_up) // Full-power mode
+                power = 1.0;
+
+
+            // If the right trigger is pressed down past a threshhold, the intake will run at full
+            // power. If left trigger, will run the opposite direction to clear the robot.
+            // If neither condition is met, automatically set power to 0 and rest.
+            if (gamepad1.right_trigger > .5) intake.setPower(-.8); // Intake in
+            else if (gamepad1.left_trigger > .5) intake.setPower(.5); // Intake out
+            else intake.setPower(0);
+
+            // Outake
+            if (gamepad1.a) outake.setPower(.7); // Outake forward
+            else if (gamepad1.b) outake.setPower(-.7); //Outake reverse
+            else outake.setPower(0);
+
+
+            // If the left bumper is pressed down past a threshhold, the carousel will run at half
+            // power. If right bumper, will run the opposite direction to spin the carousel.
+            // If neither condition is met, automatically set power to 0 and rest.
+            if(gamepad1.left_bumper) {
+                spin.setPower(.45);
+                fL.setPower(.05);
+                fR.setPower(.05);
+                bL.setPower(.05);
+                bR.setPower(.05);
+            }
+            else if (gamepad1.right_bumper) {
+                spin.setPower(-.45);
+                fL.setPower(.05);
+                fR.setPower(.05);
+                bL.setPower(.05);
+                bR.setPower(.05);
+            }
+            else spin.setPower(0);
+
+            // Either adjust the servo to sort into the box or into the base outtake
+            if (gamepad1.x) sort.setPosition(.45); // Sorting into box
+            if (gamepad1.y) sort.setPosition(.7); // Sorting into base outtake
+
+
+            //Driver 2
+            /*
+            Driver 2 currently controls the arm and wrist outtake as these mechanisms should be
+            operated while moving in order to reduce cycle times as much as possible. Although few
+            in terms of controls, Driver 2 is still vital in watching and commanding other robots
+            as well as keeping an eye out as Driver 1 will likely be focused on the main overall
+            game plan while still outtaking.
+//////////////////////////////////////////////////////////////////////////////////////////////////
+            POSSIBLE FUTURE CHANGES:
+            Test and set TRUE position for servo wrist. Possibly servo programmer.
+             */
+
+            // If the right trigger of Driver 2 is pressed past a threshold, the arm will extend out,
+            // if the left trigger is pressed, the arm will retract back down. Otherwise rest.
+            // Will likely combine with wrist movement and into different levels.
+
+            // Lift Macro
+            switch(liftState) {
+                case("raise"): {
+                    wrist.setPosition(wristTilt); // Tilt box back
+                    arm.setPower(1); // Lift arm, increase encoder
+                    if (arm.getCurrentPosition() > liftMax) { // Reaches top
+                        arm.setPower(0); // Stop lift
+                    }
+                    break;
+                }
+                case("deposit"): {
+                    wrist.setPosition(wristDeposit); // Deposit, Box points straight down
+                    if (runtime.time() > 1) { // Allows a second to allow freight to deposit
+                        wrist.setPosition(wristRest); // Tilts box back to resting position
+
+                    }
+                    break;
+                }
+
+                case("retract"): {
+                    arm.setPower(-.7); // Retract lift
+                    wrist.setPosition(wristRest);
+                    if (arm.getCurrentPosition() < liftMin) { // Checks if lift at bottom
+                        arm.setPower(0); // Stops lift
+                        liftState = "down"; // Change case back to resting
+                    }
+                    break;
+                }
+
+                default: { // Should never be reached as liftState will never be NULL
+                    liftState = "down";
+                }
+            }
+
+            if (gamepad2.right_trigger > 0.5 && arm.getCurrentPosition() > 20) arm.setPower(-.7);
+            else if (gamepad2.left_trigger > 0.5 && arm.getCurrentPosition() < 1350) arm.setPower(1);
+            else if (liftState == "down") arm.setPower(0);
+
+            if (gamepad2.dpad_up) liftState = "raise"; // Part 1, Begin lift sequence
+            if (gamepad2.dpad_right) {
+                runtime.reset(); // Reset time, used for depositing
+                liftState = "deposit"; //  Part 2, Deposit the freight and reset box
+            }
+            if (gamepad2.dpad_down) liftState = "retract"; // Part 3, retract
+
+            if (gamepad2.a) wrist.setPosition(wristRest); // Wrist Rest
+            if (gamepad2.b) wrist.setPosition(wristDeposit); // Wrist Deposit
+            if (gamepad2.y) wrist.setPosition(wristTilt); // Wrist Tilt
+            if (gamepad2.x) wrist.setPosition(wristGround); // Box points straight down
+            // Currently rotates the box either upright, tilt back slightly, or deposit element
+
+
+            // Shipping arm rotates
+            // CURRENTLY ONLY FOR TESTING, NOT READY FOR GAME USE
+            //    if (gamepad2.dpad_down) shippingArm.setPosition(.8);
+            //  else if (gamepad2.dpad_up) shippingArm.setPosition(.6);
+
+            // Hook rotates
+            // Left most position hooks
+            // CURRENTLY ONLY FOR TESTING, NOT READY FOR GAME USE
+            if (gamepad2.dpad_left) {
+                hPos += .05;
+                hook.setPosition(hPos);
+            }
+            else if (gamepad2.dpad_right) {
+                hPos -= .05;
+                hook.setPosition(hPos);
+            }
+
+
+            // Show the arm encoder position and wheel power.
+            telemetry.addData("Pulley Encoder", arm.getCurrentPosition());
+            telemetry.addData("fR:", fR.getPower());
+            telemetry.addData("fL:", fL.getPower());
+            telemetry.addData("bR:", bR.getPower());
+            telemetry.addData("bL:", bL.getPower());
+            /*telemetry.addData("sArm pos:", shippingArm.getPosition());
+            telemetry.addData("hArm pos:", hook.getPosition());
+            telemetry.addData("sPos:", sPos);
+            telemetry.addData("hPos:", hPos);*/
+            telemetry.addData("State: ", liftState);
+            telemetry.update();
+        }
+    }
+}
