a58cbcc7a81bf63d2f642593b321b136ab20e7bb
HenryRal <113390911+HenryRal@users.noreply.github.com>
2023-01-05 16:19:38 -0600

    hambu5rfwed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltagsnotclickbait/AprilTagAutonomousInitDetectionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltagsnotclickbait/AprilTagAutonomousInitDetectionExample.java
@@ -23,7 +23,7 @@ package org.firstinspires.ftc.teamcode.apriltagsnotclickbait;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -38,7 +38,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import java.util.ArrayList;
 
-@TeleOp
+@Autonomous
 public class AprilTagAutonomousInitDetectionExample extends LinearOpMode
 {
     OpenCvCamera camera;
